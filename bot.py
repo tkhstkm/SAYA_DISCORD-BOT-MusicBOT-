a0bab3171eb36d8644864be593c4875fdf729511
tkhstkm <138413285+tkhstkm@users.noreply.github.com>
2023-08-18 16:14:51 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,11 +4,14 @@ import yt_dlp
 import asyncio
 from discord.ext import commands
 
-TOKEN = ''
+TOKEN = 'discord_token'
+AUTHORIZED_USER_ID = owner_user_id
 
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
+
+music_queue = []
 
 @bot.event
 async def on_ready():
@@ -25,43 +28,58 @@ async def leave(ctx):
 
 @bot.command()
 async def play(ctx, url):
-    filepath_without_extension = "/home/ubuntu/temp"
+    global music_queue
+    music_queue.append(url)
+    if not ctx.voice_client.is_playing():
+        await play_next(ctx)
 
-    ydl_opts = {
-        'format': 'bestaudio/best',
-        'outtmpl': filepath_without_extension,
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '192',
-        }],
-    }
+@bot.command()
+async def queue(ctx):
+    queue_str = "\n".join([f"{idx+1}. {url}" for idx, url in enumerate(music_queue)])
+    await ctx.send(f"待機中の楽曲:\n{queue_str}")
 
-    # 既存のファイルがあれば削除
-    if os.path.exists(filepath_without_extension + ".mp3"):
-        os.remove(filepath_without_extension + ".mp3")
+@bot.command()
+async def shutdown(ctx):
+    if ctx.author.id == AUTHORIZED_USER_ID:
+        await bot.close()
+    else:
+        await ctx.send("シャットダウン権限がありません。")
 
-    # ダウンロード
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
+async def play_next(ctx):
+    global music_queue
+    if music_queue:
+        url = music_queue.pop(0)
+        filepath_without_extension = "/home/ubuntu/temp"
 
-    # 再生の準備
-    vc = ctx.voice_client
-    if vc is None:
-        channel = ctx.message.author.voice.channel
-        vc = await channel.connect()
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'outtmpl': filepath_without_extension,
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }],
+        }
 
-    # 再生
-    vc.play(discord.FFmpegPCMAudio(filepath_without_extension + ".mp3"), after=lambda e: print('done', e))
-    while vc.is_playing():
-        await asyncio.sleep(1)
+        if os.path.exists(filepath_without_extension + ".mp3"):
+            os.remove(filepath_without_extension + ".mp3")
 
-    # 停止
-    vc.stop()
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
 
-    # ファイル削除
-    if os.path.exists(filepath_without_extension + ".mp3"):
-        os.remove(filepath_without_extension + ".mp3")
+        vc = ctx.voice_client
+        if vc is None:
+            channel = ctx.message.author.voice.channel
+            vc = await channel.connect()
 
+        vc.play(discord.FFmpegPCMAudio(filepath_without_extension + ".mp3"), after=lambda e: play_next_coro(ctx))
+        while vc.is_playing():
+            await asyncio.sleep(1)
+
+        if os.path.exists(filepath_without_extension + ".mp3"):
+            os.remove(filepath_without_extension + ".mp3")
+
+def play_next_coro(ctx):
+    bot.loop.create_task(play_next(ctx))
 
 bot.run(TOKEN)
